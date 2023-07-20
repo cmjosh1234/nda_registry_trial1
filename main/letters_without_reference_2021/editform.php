644b55c0bc3ea1bab5fbfe688e65e7470dd24965
derrickatuhaise <atuhaisederrick@gmail.com>
2023-07-20 13:54:32 +0300

    updated changes

--- main/letters_without_reference_2021/editform.php
+++ main/letters_without_reference_2021/editform.php
@@ -9,27 +9,25 @@
 <form action="edit.php" method="POST">
 <input type="hidden" name="memids" value="<?php echo $id; ?>" />
 LETTER_REF<br>
-<input type="text" name="LETTER_REF" value="<?php echo $rows['LETTER_REF']; ?>" /><br><br>
+<input type="text" name="LETTER REF" value="<?php echo $rows['LETTER_REF']; ?>" /><br><br>
 ADDRESSED_TO<br>
-<input type="text" name="ADDRESSED_TO" value="<?php echo $rows['ADDRESSED_TO']; ?>" /><br><br>
+<input type="text" name="ADDRESSED TO" value="<?php echo $rows['ADDRESSED_TO']; ?>" /><br><br>
 SUBJECT<br>
 <input type="text" name="SUBJECT" value="<?php echo $rows['SUBJECT']; ?>" /><br><br>
 DATE_OF_LETTER<br>
-<input type="text" name="DATE_OF_LETTER" value="<?php echo $rows['DATE_OF_LETTER']; ?>" /><br><br>
+<input type="text" name="DATE OF LETTER" value="<?php echo $rows['DATE_OF_LETTER']; ?>" /><br><br>
 DATE_RECIEVED_AT_REGISTRY<br>
-<input type="text" name="DATE_RECIEVED_AT_REGISTRY" value="<?php echo $rows['DATE_RECIEVED_AT_REGISTRY']; ?>" /><br><br>
+<input type="text" name="DATE RECIEVED AT REGISTRY" value="<?php echo $rows['DATE_RECIEVED_AT_REGISTRY']; ?>" /><br><br>
 RECIPIENT<br>
 <input type="text" name="RECIPIENT" value="<?php echo $rows['RECIPIENT']; ?>" /><br><br>
 DATE_DELIVERED<br>
-<input type="text" name="DATE_DELIVERED" value="<?php echo $rows['DATE_DELIVERED']; ?>" /><br><br>
+<input type="text" name="DATE DELIVERED" value="<?php echo $rows['DATE_DELIVERED']; ?>" /><br><br>
 FILE NAME<br>
 <input type="text" name="FILE NAME" value="<?php echo $rows['FILE NAME']; ?>" /><br><br>
 FILE NO<br>
 <input type="text" name="FILE NO" value="<?php echo $rows['FILE NO']; ?>" /><br><br>
-RECEIVED AT REGISTRY<br>
-<input type="text" name="DATE_RECIEVED_AT_REGISTRY" value="<?php echo $rows['DATE_RECIEVED_AT_REGISTRY']; ?>" /><br><br>
 BOX_NO<br>
-<input type="text" name="BOX_NO" value="<?php echo $rows['BOX_NO']; ?>" /><br><br>
+<input type="text" name="BOX NO" value="<?php echo $rows['BOX_NO']; ?>" /><br><br>
 <input type="submit" value="Save" />
 </form>
 <?php
