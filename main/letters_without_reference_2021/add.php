8dce8574964222a93c1205df741045014b563c69
derrickatuhaise <atuhaisederrick@gmail.com>
2023-07-20 08:12:24 +0300

    letters_without_reference initial commit

---
+++ main/letters_without_reference_2021/add.php
@@ -0,0 +1,24 @@
+<p>ENTER NEW RECORD</p>
+<form action="reg.php" method="POST">
+LETTER_REF<br>
+<input type="text" name="LETTER_REF" /><br><br>
+ADDRESSED_TO<br>
+<input type="text" name="ADDRESSED_TO" /><br><br>
+SUBJECT<br>
+<input type="text" name="SUBJECT" /><br><br>
+DATE_OF_LETTER<br>
+<input type="text" name="DATE_OF_LETTER" /><br><br>
+DATE_RECIEVED_AT_REGISTRY<br>
+<input type="text" name="DATE_RECIEVED_AT_REGISTRY" /><br><br>
+RECIPIENT<br>
+<input type="text" name="RECIPIENT" /><br><br>
+DATE_DELIVERED<br>
+<input type="text" name="DATE_DELIVERED" /><br><br>
+FILE NAME<br>
+<input type="text" name="FILE NAME" /><br><br>
+FILE NO<br>
+<input type="text" name="FILE NO" /><br><br>
+BOX NO<br>
+<input type="text" name="BOX_NO" /><br><br>
+<input type="submit" value="Save" />
+</form>
---
+++ main/letters_without_reference_2021/index.php
@@ -0,0 +1,99 @@
+<script src="argiepolicarpio.js" type="text/javascript" charset="utf-8"></script>
+<script src="js/application.js" type="text/javascript" charset="utf-8"></script>
+<!--sa poip up-->
+<link href="style.css" rel="stylesheet" type="text/css" />
+<link href="src/facebox.css" media="screen" rel="stylesheet" type="text/css" />
+   <script src="lib/jquery.js" type="text/javascript"></script>
+  <script src="src/facebox.js" type="text/javascript"></script>
+  <script type="text/javascript">
+    jQuery(document).ready(function($) {
+      $('a[rel*=facebox]').facebox({
+        loadingImage : 'src/loading.gif',
+        closeImage   : 'src/closelabel.png'
+      })
+    })
+  </script>
+<div id="log">
+INCOMING 2021 | <a href="letters_without_reference_2021.php">LETTERS WITHOUT REFERENCE 2021</a> | <a href="paste_errors.php">PASTE ERRORS</a> | <a href="../index.php">Logout</a>
+</div>
+<div id="formdesign">
+<input type="text" name="filter" value="" id="filter" placeholder="Search Record..." autocomplete="off" />
+<a rel="facebox" href="add.php" id="add">ADD RECORD</a>
+</div>
+<table cellspacing="0" cellpadding="2" id="resultTable">
+<thead>
+	<tr>
+		<th width="5%"> LETTER_REF</th>
+		<th width="7%"> ADDRESSED TO</th>
+		<th width="10%"> SUBJECT</th>
+		<th width="10%"> DATE OF LETTER</th>
+		<th width="23%"> DATE RECIEVED AT REGISTRY</th>
+		<th width="10%"> RECIPIENT</th>
+		<th width="5%"> DATE DELIVERED</th>
+		<th width="10%"> FILE NAME </th>
+		<th width="10%"> FILE NO </th>
+		<th width="10%"> BOX NO </th>
+	</tr>
+</thead>
+<tbody>
+	<?php
+		include('connect.php');		
+		$result = $db->prepare("SELECT * FROM letters_without_reference "); //removed ORDER BY id DESC
+		$result->execute();
+		for($i=0; $row = $result->fetch(); $i++){
+	?>
+	<tr class="letters_without_reference"><!-- changed record to incoming2021 -->
+		<td><?php echo $row['LETTER_REF']; ?></td>
+		<td><?php echo $row['ADDRESSED TO']; ?></td>
+		<td><?php echo $row['SUBJECT']; ?></td>
+		<td><?php echo $row['DATE OF LETTER']; ?></td>
+		<td><?php echo $row['DATE RECIEVED AT REGISTRY']; ?></td>
+		<td><?php echo $row['RECIPIENT']; ?></td>
+		<td><?php echo $row['DATE_DELIVERED']; ?></td>
+		<td><?php echo $row['FILE_NAME']; ?></td>
+		<td><?php echo $row['FILE_NO']; ?></td>
+		<td><?php echo $row['BOX_NO']; ?></td>
+		<td><a rel="facebox" href="editform.php?id=<?php echo $row['id']; ?>"> Edit </a> | <a href="#" id="<?php echo $row['id']; ?>" class="delbutton" title="Click To Delete">Delete</a></td>
+	</tr>
+	<?php
+		}
+	?>
+</tbody>
+</table>
+<script src="js/jquery.js"></script>
+  <script type="text/javascript">
+$(function() {
+
+
+$(".delbutton").click(function(){
+
+//Save the link in a variable called element
+var element = $(this);
+
+//Find the id of the link that was clicked
+var del_id = element.attr("id");
+
+//Built a url to send
+var info = 'id=' + del_id;
+ if(confirm("Sure you want to delete this update? There is NO undo!"))
+		  {
+
+ $.ajax({
+   type: "GET",
+   url: "delete.php",
+   data: info,
+   success: function(){
+   
+   }
+ });
+         $(this).parents(".nda_registry_trial1").animate({ backgroundColor: "#fbc7c7" }, "fast")//removed record, addes nda_registry_trial1
+		.animate({ opacity: "hide" }, "slow");
+
+ }
+
+return false;
+
+});
+
+});
+</script>
